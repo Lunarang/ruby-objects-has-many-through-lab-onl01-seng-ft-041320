b9c2c68e2f618768286051141d2092d47ed9274e
Erin Mettille <ecmettille@gmail.com>
2020-04-29 23:24:49 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,16 @@
 class Patient
+  attr_reader :name
+  @@all = []
   
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def new_appointment(date, doctor)
+    Appointment.new(date,)
 end
