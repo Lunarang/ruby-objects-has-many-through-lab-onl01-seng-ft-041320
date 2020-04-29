c2a1a3e668dd6beae3de4b2fb0a1b71dce04498e
Erin Mettille <ecmettille@gmail.com>
2020-04-29 23:12:13 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,23 @@
-class
+class Genre 
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def songs 
+    Songs.all.select {|song| song.genre == self}
+  end 
+  
+  def artists
+    self.songs.collect {|song| song.artist}
+  end 
+  
+end
